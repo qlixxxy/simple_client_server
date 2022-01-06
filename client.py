b86ac65cf2f573ba0ad61d4d494af874ad81e09c
qlixxxy <sqlixor@gmail.com>
2022-01-07 02:08:03 +0300

    done

--- client.py
+++ client.py
@@ -17,18 +17,22 @@ class Client:
             client.connect((server_ip, server_port))
         for index, client in enumerate(self.clients):
             if text:
-                filename += f" I am the {index} Client\n"
-            client.sendall(filename.encode(encoding="utf-8"))
-        for index, client in enumerate(self.clients):
-            server_response = client.recv(1024).decode("utf-8")
-            if self.server_responses.get(index, None):
-                self.server_responses[index].append(server_response)
+                request = filename + f" I am the {index} Client\n"
             else:
-                self.server_responses[index] = [server_response]
+                request = filename
+            client.send(request.encode(encoding="utf-8"))
+        for index, client in enumerate(self.clients):
+            while True:
+                server_response = client.recv(1024).decode("utf-8")
+                if not server_response:
+                    break
+                if self.server_responses.get(index, None):
+                    self.server_responses[index].append(server_response)
+                else:
+                    self.server_responses[index] = [server_response]
         for index, info in self.server_responses.items():
-            info = ''.join(info)
+            info = "".join(info)
             print(f'Client {index} got "{info}" message from server')
-                
 
 s = Client(10)
 s.get_request("127.0.0.1", 6060, "groovestreet.txt", True)
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import select
-from time import sleep
 
 
 class Server:
@@ -9,13 +8,12 @@ class Server:
     This class takes two arguments: a host address and a port to initialize the socket
     """
 
-    _package_size = 2048
+    _package_size = 1024
 
     def __init__(self, server_ip: str, server_port: int):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((server_ip, server_port))
-        self.server.listen(100)
+        self.server.listen(2000)
         self.server.setblocking(False)
         self.input = [self.server]
         self.output = []
@@ -27,6 +25,9 @@ class Server:
             to_read, to_send, errors = select.select(
                 self.input, self.output, self.errors
             )
+            if not to_read:
+                print("Server is in offline mode")
+                break
             for connection in to_read:
                 if connection == self.server:
                     new_connection, addr = connection.accept()
@@ -34,12 +35,25 @@ class Server:
                     new_connection.setblocking(False)
                     self.input.append(new_connection)
                 else:
-                    data_received = connection.recv(self._package_size).decode("utf-8")
-                    if data_received:
-                        if self.buffer.get(connection, None):
-                            self.buffer[connection].append(data_received)
-                        else:
-                            self.buffer[connection] = [data_received]
+                    collected_data = []
+                    while True:
+                        try:
+                            data_received = connection.recv(self._package_size).decode(
+                                "utf-8"
+                            )
+                        except BlockingIOError:
+                            break
+                        collected_data.append(data_received)
+                    if collected_data:
+                        file = "".join(collected_data).split(" ")
+                        collected_data.clear()
+                        server_response = self.file_handler(file)
+                        descriptor = connection.fileno()
+                        try:
+                            self.buffer[descriptor].append(server_response)
+                        except KeyError:
+                            self.buffer[descriptor] = [server_response]
+                        self.input.remove(connection)
                         if connection not in self.output:
                             self.output.append(connection)
                     else:
@@ -47,31 +61,31 @@ class Server:
                             self.output.remove(connection)
                         self.input.remove(connection)
                         connection.close()
-                        del self.buffer[connection]
             for connection in to_send:
-                client_message = self.buffer.get(connection)
-                if client_message:
-                    client_message = " ".join(client_message).split(" ")
-                    client_message = self.file_handler(client_message)
-                    connection.send(client_message)
-                else:
+                descriptor = connection.fileno()
+                client_message = "".join(self.buffer[descriptor])
+                del self.buffer[descriptor]
+                connection.send(client_message.encode(encoding="utf-8"))
+                if connection in self.output:
                     self.output.remove(connection)
+                connection.close()
             for connection in errors:
                 self.input.remove(connection)
+                if connection in self.output:
+                    self.output.remove(connection)
                 connection.close()
-                del self.buffer[connection]
 
     def file_handler(self, file):
         if len(file) == 1:
             with open(file[0], "r") as f:
                 request_data = f.read().splitlines()
-            data_to_send = " ".join(request_data).encode(encoding="utf-8")
+            data_to_send = " ".join(request_data)
             return data_to_send
         elif len(file) > 1:
-            with open(file[0], "w") as f:
+            with open(file[0], "a") as f:
                 text_to_write = " ".join(file[1:])
-                f.write(text_to_write) 
-            data_to_send = 'Changes accepted'.encode(encoding="utf-8")
+                f.write(text_to_write)
+            data_to_send = "Changes accepted"
             return data_to_send
 
     def console_formating(self, address: str):
@@ -80,7 +94,6 @@ class Server:
             separator, address[0], address[1], separator
         )
         print(formatted_text)
-
 
 server_1 = Server("127.0.0.1", 6060)
 server_1.receiving_mode()
